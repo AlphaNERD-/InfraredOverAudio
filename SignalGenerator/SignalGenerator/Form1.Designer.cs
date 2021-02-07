a93eb62b4581fec8b0f513842d04930e63e5025b
Julia Wolf <lovelydeath94@googlemail.com>
2021-02-07 21:25:13 +0100

    Completely rewritten SignalGenerator to generate UART packages as PCM audio signals
    (based on David Wehr's work shown here: https://davidawehr.com/blog/audioserial/)

--- SignalGenerator/SignalGenerator/Form1.Designer.cs
+++ SignalGenerator/SignalGenerator/Form1.Designer.cs
@@ -33,15 +33,14 @@
             this.lblIRfrequency = new System.Windows.Forms.Label();
             this.lblIRdata = new System.Windows.Forms.Label();
             this.btnPlaySignal = new System.Windows.Forms.Button();
-            this.btnNext = new System.Windows.Forms.Button();
-            this.btnPrv = new System.Windows.Forms.Button();
             this.lblInfo = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.nudIRFrequency)).BeginInit();
             this.SuspendLayout();
             // 
             // nudIRFrequency
             // 
-            this.nudIRFrequency.Location = new System.Drawing.Point(106, 7);
+            this.nudIRFrequency.Location = new System.Drawing.Point(80, 6);
+            this.nudIRFrequency.Margin = new System.Windows.Forms.Padding(2);
             this.nudIRFrequency.Maximum = new decimal(new int[] {
             45000,
             0,
@@ -53,7 +52,7 @@
             0,
             0});
             this.nudIRFrequency.Name = "nudIRFrequency";
-            this.nudIRFrequency.Size = new System.Drawing.Size(120, 22);
+            this.nudIRFrequency.Size = new System.Drawing.Size(90, 20);
             this.nudIRFrequency.TabIndex = 0;
             this.nudIRFrequency.Value = new decimal(new int[] {
             35000,
@@ -66,84 +65,68 @@
             this.tbxIRdata.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbxIRdata.Location = new System.Drawing.Point(106, 35);
+            this.tbxIRdata.Location = new System.Drawing.Point(80, 28);
+            this.tbxIRdata.Margin = new System.Windows.Forms.Padding(2);
             this.tbxIRdata.Multiline = true;
             this.tbxIRdata.Name = "tbxIRdata";
-            this.tbxIRdata.Size = new System.Drawing.Size(477, 176);
+            this.tbxIRdata.Size = new System.Drawing.Size(359, 144);
             this.tbxIRdata.TabIndex = 1;
-            this.tbxIRdata.Text = "+100 +200 +300 +400 +500 +600 +700 +800 +900 +1000";
+            this.tbxIRdata.Text = "+100";
             // 
             // lblIRfrequency
             // 
             this.lblIRfrequency.AutoSize = true;
-            this.lblIRfrequency.Location = new System.Drawing.Point(12, 9);
+            this.lblIRfrequency.Location = new System.Drawing.Point(9, 7);
+            this.lblIRfrequency.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblIRfrequency.Name = "lblIRfrequency";
-            this.lblIRfrequency.Size = new System.Drawing.Size(88, 17);
+            this.lblIRfrequency.Size = new System.Drawing.Size(68, 13);
             this.lblIRfrequency.TabIndex = 2;
             this.lblIRfrequency.Text = "IR frequency";
             // 
             // lblIRdata
             // 
             this.lblIRdata.AutoSize = true;
-            this.lblIRdata.Location = new System.Drawing.Point(12, 38);
+            this.lblIRdata.Location = new System.Drawing.Point(9, 31);
+            this.lblIRdata.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblIRdata.Name = "lblIRdata";
-            this.lblIRdata.Size = new System.Drawing.Size(53, 17);
+            this.lblIRdata.Size = new System.Drawing.Size(42, 13);
             this.lblIRdata.TabIndex = 3;
             this.lblIRdata.Text = "IR data";
             // 
             // btnPlaySignal
             // 
             this.btnPlaySignal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnPlaySignal.Location = new System.Drawing.Point(474, 217);
+            this.btnPlaySignal.Location = new System.Drawing.Point(356, 176);
+            this.btnPlaySignal.Margin = new System.Windows.Forms.Padding(2);
             this.btnPlaySignal.Name = "btnPlaySignal";
-            this.btnPlaySignal.Size = new System.Drawing.Size(109, 27);
+            this.btnPlaySignal.Size = new System.Drawing.Size(82, 22);
             this.btnPlaySignal.TabIndex = 4;
             this.btnPlaySignal.Text = "Play Signal";
             this.btnPlaySignal.UseVisualStyleBackColor = true;
             this.btnPlaySignal.Click += new System.EventHandler(this.btnPlaySignal_Click);
             // 
-            // btnNext
-            // 
-            this.btnNext.Location = new System.Drawing.Point(292, 217);
-            this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(75, 27);
-            this.btnNext.TabIndex = 5;
-            this.btnNext.Text = "Next";
-            this.btnNext.UseVisualStyleBackColor = true;
-            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
-            // 
-            // btnPrv
-            // 
-            this.btnPrv.Location = new System.Drawing.Point(106, 217);
-            this.btnPrv.Name = "btnPrv";
-            this.btnPrv.Size = new System.Drawing.Size(75, 27);
-            this.btnPrv.TabIndex = 6;
-            this.btnPrv.Text = "Previous";
-            this.btnPrv.UseVisualStyleBackColor = true;
-            this.btnPrv.Click += new System.EventHandler(this.btnPrv_Click);
-            // 
             // lblInfo
             // 
             this.lblInfo.AutoSize = true;
-            this.lblInfo.Location = new System.Drawing.Point(187, 222);
+            this.lblInfo.Location = new System.Drawing.Point(9, 181);
+            this.lblInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblInfo.Name = "lblInfo";
-            this.lblInfo.Size = new System.Drawing.Size(67, 17);
+            this.lblInfo.Size = new System.Drawing.Size(52, 13);
             this.lblInfo.TabIndex = 7;
             this.lblInfo.Text = "Waiting...";
             // 
             // frmIRSignalGenerator
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(590, 256);
+            this.ClientSize = new System.Drawing.Size(442, 208);
             this.Controls.Add(this.lblInfo);
-            this.Controls.Add(this.btnPrv);
-            this.Controls.Add(this.btnNext);
             this.Controls.Add(this.btnPlaySignal);
             this.Controls.Add(this.lblIRdata);
             this.Controls.Add(this.lblIRfrequency);
             this.Controls.Add(this.tbxIRdata);
             this.Controls.Add(this.nudIRFrequency);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmIRSignalGenerator";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.nudIRFrequency)).EndInit();
@@ -159,8 +142,6 @@
         private System.Windows.Forms.Label lblIRfrequency;
         private System.Windows.Forms.Label lblIRdata;
         private System.Windows.Forms.Button btnPlaySignal;
-        private System.Windows.Forms.Button btnNext;
-        private System.Windows.Forms.Button btnPrv;
         private System.Windows.Forms.Label lblInfo;
     }
 }
--- SignalGenerator/SignalGenerator/Form1.cs
+++ SignalGenerator/SignalGenerator/Form1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,177 +18,96 @@ namespace IRSignalGenerator
 {
     public partial class frmIRSignalGenerator : Form
     {
-        IRSignalGenerator gen;
         WaveOutEvent wo;
 
         public frmIRSignalGenerator()
         {
             InitializeComponent();
 
-            gen = new IRSignalGenerator()
-            {
-                Gain = 0.2,
-            };
-
             wo = new WaveOutEvent();
-            wo.Init(gen);
         }
 
         private void btnPlaySignal_Click(object sender, EventArgs e)
         {
-            if (wo.PlaybackState == PlaybackState.Playing)
+            byte[] test = PrepareIRData();
+            List<byte> packets = new List<byte>();
+
+            WaveFormat waveFormat = new WaveFormat(48000, 16, 1);
+
+            foreach(byte b in test)
             {
-                wo.Stop();
-                btnPlaySignal.Text = "Play Signal";
-                lblInfo.Text = "Waiting...";
+                int oneBits = 0;
+
+                //Start Bit
+                packets.Add(Convert.ToByte(-short.MaxValue >> 8));
+                packets.Add(Convert.ToByte(-short.MaxValue & 0x00FF));
+
+                //Actual data
+                int signalIndex = 1;
+                for (int i = 0; i > 8; i++)
+                {
+                    bool bit = (b & (1 << i - 1)) != 0;
+
+                    if (bit)
+                    {
+                        oneBits++;
+                        packets.Add(Convert.ToByte(short.MaxValue >> 8));
+                        packets.Add(Convert.ToByte(short.MaxValue & 0x00FF));
+                    }
+                    else
+                    {
+                        packets.Add(Convert.ToByte(-short.MaxValue >> 8));
+                        packets.Add(Convert.ToByte(-short.MaxValue & 0x00FF));
+                    }
+
+                    signalIndex++;
+                }
+
+                //Odd Parity bit
+                if (oneBits % 2 == 0)
+                {
+                    packets.Add(Convert.ToByte(short.MaxValue >> 8));
+                    packets.Add(Convert.ToByte(short.MaxValue & 0x00FF));
+                }
+                else
+                {
+                    packets.Add(Convert.ToByte(-short.MaxValue >> 8));
+                    packets.Add(Convert.ToByte(-short.MaxValue & 0x00FF));
+                }
+
+                //Stop Bit
+                packets.Add(Convert.ToByte(short.MaxValue >> 8));
+                packets.Add(Convert.ToByte(short.MaxValue & 0x00FF));
             }
-            else
-            {
-                gen.Frequencies = PrepareIRData();
-                wo.Play();
-                btnPlaySignal.Text = "Stop Signal";
-                UpdateInfoLabel();
-            }
+
+            RawSourceWaveStream ws = new RawSourceWaveStream(new MemoryStream(packets.ToArray()), waveFormat);
+
+            wo.Init(ws);
+            wo.Play();
+
+            //gen.Frequencies = PrepareIRData();
+            //wo.Play();
+            //UpdateInfoLabel();
         }
 
-        private double[] PrepareIRData()
+        private byte[] PrepareIRData()
         {
-            double[] result = null;
+            List<byte> resultList = new List<byte>();
             string[] entries = tbxIRdata.Text.Split(' ');
             int index = 0;
 
-            //We not only need the actual IR data but also
-            //some signals to tell the Arduino what frequency
-            //to pulse the LED with. And we need to tell the
-            //Arduino when we send IR data and when we stop.
-            result = new double[entries.Length + 4];
-
-            //Set IR frequency signal
-            result[index] = 1000d;
-            index++;
-
-            //Signal the IR frequency
-            result[index] = (float)nudIRFrequency.Value - 25000;
-            index++;
-
-            //Tell Arduino listen to incoming data
-            result[index] = 2000d;
-            index++;
-
-            foreach(string entry in entries)
+            foreach (string entry in entries)
             {
                 string tempEntry = entry.Trim(' ', '+', '-');
-                //tempEntry = tempEntry.Remove(0, 1);
 
-                result[index] = Convert.ToDouble(tempEntry);
+                Int16 value = Convert.ToInt16(tempEntry);
+
+                resultList.Add(Convert.ToByte(value >> 8));
+                resultList.Add(Convert.ToByte(value & 0x00FF));
                 index++;
             }
 
-            //Tell Arduino to stop listening to incoming data
-            //And of course send the data
-            result[index] = 3000d;
-            index++;
-
-            return result;
-        }
-
-        private void UpdateInfoLabel()
-        {
-            lblInfo.Text = "(" + (gen.currentFrequency + 1) + "/" + gen.Frequencies.Length + "), " + gen.Frequencies[gen.currentFrequency] + " Hz";
-        }
-
-        private void btnNext_Click(object sender, EventArgs e)
-        {
-            if(gen.currentFrequency < gen.Frequencies.Length - 1)
-            {
-                gen.currentFrequency++;
-                UpdateInfoLabel();
-            }
-        }
-
-        private void btnPrv_Click(object sender, EventArgs e)
-        {
-            if (gen.currentFrequency > 0)
-            {
-                gen.currentFrequency--;
-                UpdateInfoLabel();
-            }
-        }
-    }
-
-    /// <summary>
-    /// This class is derived from the SignalGenerator class of the NAudio project.
-    /// </summary>
-    public class IRSignalGenerator : ISampleProvider
-    {
-        // Wave format
-        private readonly WaveFormat waveFormat;
-
-        // Generator variable
-        private int nSample;
-
-        /// <summary>
-        /// Initializes a new instance for the Generator
-        /// </summary>
-        /// <param name="sampleRate">Desired sample rate</param>
-        /// <param name="channel">Number of channels</param>
-        public IRSignalGenerator()
-        {
-            waveFormat = WaveFormat.CreateIeeeFloatWaveFormat(44100, 1);
-
-            // Default
-            Gain = 1;
-        }
-
-        /// <summary>
-        /// The waveformat of this WaveProvider (same as the source)
-        /// </summary>
-        public WaveFormat WaveFormat => waveFormat;
-
-        /// <summary>
-        /// Array of frequencies for the Generator. (20.0 - 20000.0 Hz)
-        /// </summary>
-        public double[] Frequencies { get; set; }
-
-        public int currentFrequency;
-
-        /// <summary>
-        /// Gain for the Generator. (0.0 to 1.0)
-        /// </summary>
-        public double Gain { get; set; }
-
-        /// <summary>
-        /// Reads from this provider.
-        /// </summary>
-        public int Read(float[] buffer, int offset, int count)
-        {
-            int outIndex = offset;
-
-            // Generator current value
-            double multiple;
-            double sampleValue;
-            double sampleSaw;
-
-            // Complete Buffer
-            for (int sampleCount = 0; sampleCount < count; sampleCount++)
-            {
-                multiple = 2 * Frequencies[currentFrequency] / waveFormat.SampleRate;
-                sampleSaw = ((nSample * multiple) % 2) - 1;
-                sampleValue = sampleSaw > 0 ? Gain : -Gain;
-
-                nSample++;
-
-                /*if (sampleCount % 1000 == 0)
-                {
-                    currentFrequency++;
-
-                    if (currentFrequency == Frequencies.Length)
-                        currentFrequency = 0;
-                }*/
-
-                buffer[outIndex++] = (float)sampleValue;
-            }
-            return count;
+            return resultList.ToArray();
         }
     }
 }
