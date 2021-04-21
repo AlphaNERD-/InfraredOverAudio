5ae1c257c610a21484d90d304eb4ef777d41fe14
Kyra Zimmer <me@nonchip.de>
2021-04-21 12:32:40 +0200

    refactored into bell202.h, added neat get_sign function

--- AudioCarrier/AudioCarrier.ino
+++ AudioCarrier/AudioCarrier.ino
@@ -1,69 +1,24 @@
 #include <Arduino.h>
-#include <util/atomic.h>
-
-volatile uint16_t ringbuffer[8];
-volatile uint8_t ringbuffer_index=0;
-const static int8_t coeffloi[] = { 64,  45,   0, -45, -64, -45,   0,  45};
-const static int8_t coeffloq[] = {  0,  45,  64,  45,   0, -45, -64, -45};
-const static int8_t coeffhii[] = { 64,   8, -62, -24,  55,  39, -45, -51};
-const static int8_t coeffhiq[] = {  0,  63,  17, -59, -32,  51,  45, -39};
-
-volatile int32_t current_sign = 0;
-
-ISR(TIMER1_COMPA_vect) { // 9603.84Hz
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-    ringbuffer[ringbuffer_index++]=analogRead(A0);
-    if(ringbuffer_index>=8)
-      ringbuffer_index=0;
-  }
-  int16_t outloi = 0, outloq = 0, outhii = 0, outhiq = 0;
-  for (int i = 0; i < 8; i++) {
-    int8_t sample = (ringbuffer[(i+ringbuffer_index)%8] - 512)>>2;
-    outloi += sample * coeffloi[i];
-    outloq += sample * coeffloq[i];
-    outhii += sample * coeffhii[i];
-    outhiq += sample * coeffhiq[i];
-  }
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-    current_sign = ((outhii >> 8) * (outhii >> 8) + (outhiq >> 8) * (outhiq >> 8)
-                 - (outloi >> 8) * (outloi >> 8) - (outloq >> 8) * (outloq >> 8));
-  }
-}
-
-void setupTimer1() {
-  noInterrupts();
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCNT1 = 0;
-  // 9603.841536614646 Hz=(16000000/((1665+1)*1))
-  OCR1A = 1665;
-  // CTC
-  TCCR1B |= (1 << WGM12);
-  // Prescaler 1
-  TCCR1B |= (1 << CS10);
-  // Output Compare Match A Interrupt Enable
-  TIMSK1 |= (1 << OCIE1A);
-  interrupts();
-}
-
+#include "bell202.h"
 
 void setup(){
-  pinMode(A0,INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  ADCSRA = (ADCSRA & B11111000) | 2;
-  setupTimer1();
+  bell202::setup(A0);
 }
 
-uint8_t prev_idx=0;
 void loop(){
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-    if(ringbuffer_index!=prev_idx){
-      if(current_sign>0){
-        digitalWrite(LED_BUILTIN,HIGH);
-      }else{
-        digitalWrite(LED_BUILTIN,LOW);
-      }
-      prev_idx = ringbuffer_index;
+  int32_t sign = bell202::get_sign();
+
+  if(sign==0) // no carrier accumulated (yet)
+    return;
+
+  if(abs(sign)>1000){ // sign loud enough
+    if(sign>0){ // mark = 2200Hz
+      digitalWrite(LED_BUILTIN,HIGH);
+    }else{      // space = 1200Hz
+      digitalWrite(LED_BUILTIN,LOW);
     }
+  }else{ // silence
+
   }
 }
---
+++ AudioCarrier/bell202.h
@@ -0,0 +1,80 @@
+#include <Arduino.h>
+#include <util/atomic.h>
+
+namespace bell202 {
+
+  int readpin = A0;
+
+  volatile uint16_t ringbuffer[8];
+  volatile uint8_t ringbuffer_index=0;
+  const static int8_t coeffloi[] = { 64,  45,   0, -45, -64, -45,   0,  45};
+  const static int8_t coeffloq[] = {  0,  45,  64,  45,   0, -45, -64, -45};
+  const static int8_t coeffhii[] = { 64,   8, -62, -24,  55,  39, -45, -51};
+  const static int8_t coeffhiq[] = {  0,  63,  17, -59, -32,  51,  45, -39};
+
+  volatile int32_t current_sign = 0;
+  volatile int32_t accumulated_sign=0;
+  volatile bool have_full_carrier=false;
+
+  ISR(TIMER1_COMPA_vect) { // 9603.84Hz
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+      ringbuffer[ringbuffer_index++]=analogRead(readpin);
+      if(ringbuffer_index>=8){
+        ringbuffer_index=0;
+        have_full_carrier = true;
+      }
+    }
+    int16_t outloi = 0, outloq = 0, outhii = 0, outhiq = 0;
+    for (int i = 0; i < 8; i++) {
+      int8_t sample = (ringbuffer[(i+ringbuffer_index)%8] - 512)>>2;
+      outloi += sample * coeffloi[i];
+      outloq += sample * coeffloq[i];
+      outhii += sample * coeffhii[i];
+      outhiq += sample * coeffhiq[i];
+    }
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+      current_sign = ((outhii >> 8) * (outhii >> 8) + (outhiq >> 8) * (outhiq >> 8)
+                   - (outloi >> 8) * (outloi >> 8) - (outloq >> 8) * (outloq >> 8));
+      accumulated_sign += current_sign / 8;
+    }
+  }
+
+  int32_t get_sign(){
+    int32_t ret;
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+      if(!have_full_carrier)
+        return 0;
+      ret = accumulated_sign;
+      accumulated_sign = 0;
+    }
+    if(ret==0)
+      ret=-1;
+    return ret;
+  }
+
+  void setup(int p) {
+    noInterrupts();
+
+    // setup readpin
+
+    readpin = p;
+    pinMode(p,INPUT);
+    ADCSRA = (ADCSRA & B11111000) | 2;
+
+    // setup timer
+
+    TCCR1A = 0;
+    TCCR1B = 0;
+    TCNT1 = 0;
+    // 9603.841536614646 Hz=(16000000/((1665+1)*1))
+    OCR1A = 1665;
+    // CTC
+    TCCR1B |= (1 << WGM12);
+    // Prescaler 1
+    TCCR1B |= (1 << CS10);
+    // Output Compare Match A Interrupt Enable
+    TIMSK1 |= (1 << OCIE1A);
+    interrupts();
+  }
+
+};
