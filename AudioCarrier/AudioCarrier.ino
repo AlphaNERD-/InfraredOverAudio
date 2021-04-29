48830366e9a875db7a2a92c7f98b41032455aa89
Kyra Zimmer <me@nonchip.de>
2021-04-29 17:34:23 +0200

    made debug pin stuff

--- AudioCarrier/AudioCarrier.ino
+++ AudioCarrier/AudioCarrier.ino
@@ -8,15 +8,22 @@ uint8_t currentByteIndex = 0;
 void setup(){
   pinMode(LED_BUILTIN, OUTPUT);
   bell202::setup(A0);
+
+  pinMode(3,OUTPUT); // Clock (data stable when high)
+  pinMode(4,OUTPUT); // Data
 }
 
 void loop(){
+  digitalWrite(3,LOW);
   int32_t sign = bell202::get_sign();
 
   if(sign==0) // no carrier accumulated (yet)
     return;
 
-  if(abs(sign)>1000){ // sign loud enough
+  if(abs(sign)>100){ // sign loud enough
+    digitalWrite(4,sign>0);
+    digitalWrite(3,HIGH);
+
     if(sign < 0 && !recordByte){
       recordByte = true;
     }else{
@@ -24,15 +31,13 @@ void loop(){
       {
         recordByte = false;
         currentByteIndex = 0;
-
-        Serial.begin(115200);
-        Serial.println(currentByte, HEX);
-        Serial.end();
       }else{
-        currentByte = (currentByte >> 1) | ((sign>0)<<7);
+        currentByte = (currentByte << 1) | (sign>0)&1;
       }
     }
+
   }else{ // silence
+    recordByte = false;
 
   }
 }
